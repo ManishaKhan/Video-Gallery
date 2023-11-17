cd86200e08520b849ae662a4911196e613e70196
manishaKhan1 <117927495+manishaKhan1@users.noreply.github.com>
2023-11-18 00:30:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,169 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Media } from './Components';
+import { VIDEOS } from './Components';
+import { useState } from 'react'
+// import { ReactVideoGallery } from './Components/ReactVideoGallery';
+// import { VideoCarousel } from './Components/VideoCarousel';
+import ImageSlider from './Components/ImageSlider';
+
 
 function App() {
+
+  const [file, setFile] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //     <>
+    //     {/* Slider-VIdeos */}
+
+    //     <div className='App'>
+    //       <h1> 📽🎥 Video Galley 🎥📽 </h1>
+    //       <div className='container'>
+    //         <ImageSlider images={VIDEOS} />
+
+    //       </div>
+
+    //     </div>
+
+
+    // {/* pop-up videos */}
+    //     <div className="container">
+
+    //       <div className='media-container'>
+
+    //         {
+    //           Media.map((file, index)=>(
+    //             <div className="media" key={index} 
+    //             onClick={()=>setFile(file)}>
+    //               {
+    //                 <video src={file.url}></video>
+
+    //               }
+
+    //             </div>
+    //           ))
+    //         }
+
+    //       </div>
+
+    //       {/* pop-up-video  */}
+    //       <div className='popup-media' style={{ display: file ? 'block' : 'none' }}>
+    //         <span onClick={() => setFile(null)}>&times;</span>
+    //         {
+    //           <video src={file?.url} muted autoPlay controls></video>
+    //         }
+    //       </div>
+    //     </div>
+    //     </>
+
+
+
+    <>
+
+
+      <div className='App'>
+        {/* <div className='nine'>
+        <h1>  <span>🎥📽📽🎥</span> Video Galley  <span>🎥📽📽🎥</span></h1>
+        </div> */}
+        <div class="wrapper">
+          <div class="bg">Video Gallery </div>
+          <div class="fg"> Video Gallery </div>
+        </div>
+        <div className='container'>
+
+          {/* Slider-VIdeos */}
+          <ImageSlider images={VIDEOS} />
+
+
+
+          {/* pop-up videos */}
+
+          <div className="container">
+            <div className='nine'>
+              <h1>Pop-up Videos <span>🎥📽📽🎥</span></h1>
+            </div>
+
+            <div className='media-container'>
+
+              {
+                Media.map((file, index) => (
+
+
+                  // <div class="menu__container">
+
+                  //   <div class="menu">
+
+                  //     <div class="stack">
+
+                  //       <div class="card">
+
+
+
+                  //         <div className="media" key={index}
+                  //           onClick={() => setFile(file)}>
+                  //           {
+                  //             <video src={file.url}></video>
+
+
+                  //           }
+
+
+
+                  //         </div>
+                  //       </div>
+                  //     </div>
+                  //   </div>
+                  // </div>
+                  <div class="menu__container">
+
+                    <div class="menu">
+
+                      <div class="box">
+
+                        <div class="card">
+                          <div class="imgBx">
+
+
+                            <div className="media" key={index}
+                              onClick={() => setFile(file)}>
+                              {
+                                <video src={file.url}></video>
+
+
+                              }
+
+
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                ))
+
+              }
+
+            </div>
+
+            {/* pop-up-video  */}
+            <div className='popup-media' style={{ display: file ? 'block' : 'none' }}>
+              <span onClick={() => setFile(null)}>&times;</span>
+              {
+                <video src={file?.url} muted autoPlay controls></video>
+              }
+            </div>
+          </div>
+
+        </div>
+
+
+      </div>
+
+
+    </>
+
   );
 }
 
---
+++ src/Components/index.js
@@ -0,0 +1,190 @@
+import video1 from "./Videos/video1.mp4"
+import video2 from "./Videos/video2.mp4"
+import video4 from "./Videos/video4.mp4"
+import video6 from "./Videos/video6.mp4"
+import video7 from "./Videos/video7.mp4"
+import video8 from "./Videos/video8.mp4"
+import video9 from "./Videos/video9.mp4"
+
+
+// Pop-up videos
+
+export const Media = [
+    {
+        type: 'video',
+        url: require('./Videos/video1.mp4'),
+
+    },
+
+
+    {
+        type: 'video',
+        url: require('./Videos/video2.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video6.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video7.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video8.mp4')
+    },
+
+    {
+        type: 'video',
+        url: require('./Videos/video9.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video10.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video11.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video4.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video12.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video13.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video14.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video15.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video16.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video4.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video17.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video4.mp4')
+    },
+    {
+        type: 'video',
+        url: require('./Videos/video18.mp4')
+    }
+    
+]
+
+
+// Slider-videos
+
+export const VIDEOS = [
+    {
+      id: 1,
+      code: (
+        <iframe
+          title="vid1"
+          width="560"
+          height="315"
+          src={video1}
+        //   allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      ),
+    },
+    {
+      id: 2,
+      code: (
+        <iframe
+          title="vid2"
+          width="560"
+          height="315"
+          src={video2}
+        //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      ),
+    },
+    {
+      id: 3,
+      code: (
+        <iframe
+          title="vid3"
+          width="560"
+          height="315"
+          src={video4}
+        //   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        ></iframe>
+      ),
+    },
+   
+      {
+        id: 6,
+        code: (
+          <iframe
+            title="vid4"
+            width="560"
+            height="315"
+            src={video6}
+            // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        ),
+      },
+      
+     
+      {
+        id: 9,
+        code: (
+          <iframe
+            title="vid4"
+            width="560"
+            height="315"
+            src={video9}
+            // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        ),
+      },
+      
+      {
+        id: 11,
+        code: (
+          <iframe
+            title="vid4"
+            width="560"
+            height="315"
+            src={video7}
+            // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        ),
+      },
+      
+      {
+        id: 13,
+        code: (
+          <iframe
+            title="vid4"
+            width="560"
+            height="315"
+            src={video8}
+            // allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          ></iframe>
+        ),
+      }
+    
+
+  ];
